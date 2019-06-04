02ab9f73b80fe2fbda45f9f2801554fa92d7b0bd
dasha-kop <testSoft-Logic@yandex.ru>
2019-06-04 13:48:06 +0700

    Инициализация проекта

---
+++ src/test/java/Tests.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnit4ClassRunner.class)
+public class Tests {
+
+    @Test
+    public void test(){
+        System.out.println("Проверка");
+    }
+}
